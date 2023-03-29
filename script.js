a8591a7a983ddc8ec84ccbc5d08e665770da56b0
onlyjustmandy <amanda_ferreira89@hotmail.com>
2023-03-28 23:11:56 -0300

    script para mudança de foto das denuncias

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-const buttons = document.querySelectorAll('.lista-denuncia button');
-const imagemGrande = document.querySelector('.imagem-grande img');
 
-buttons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    imagemGrande.src = `imagens/denuncia-${index}.jpg`;
-  });
-});
+
+function trocarFoto(src) {
+    var foto = document.getElementById('foto-denuncia');
+    foto.src = src;
+  }
