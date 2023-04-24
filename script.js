1d08e0317f336c856155dc6721c27a2768817172
onlyjustmandy <amanda_ferreira89@hotmail.com>
2023-04-23 22:16:35 -0300

    jogo da memoria

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 
-
 function trocarFoto(src) {
     var foto = document.getElementById('foto-denuncia');
     foto.src = src;
   }
+
+  
+
+  const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flipped');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.querySelector('img').src === secondCard.querySelector('img').src;
+
+  isMatch ? disableCards() : unflip
